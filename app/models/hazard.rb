6f4d994944108c734680c7e1d21c173e8f7662e0
Amy MacKinnon <amy@amy-mac.com>
2013-11-12 12:30:53 -0800

    Wrote tests for hazard model

--- app/models/hazard.rb
+++ app/models/hazard.rb
@@ -1,3 +1,8 @@
 class Hazard < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :latitude, :longitude, :description, :hazard_type, :user_id
+  
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :hazard_type, presence: true
+  validates :user_id, presence: true
 end
