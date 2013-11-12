642856a9e2c95d04ef4cb254bbf01bed255e5172
Amy MacKinnon <amy@amy-mac.com>
2013-11-12 12:30:53 -0800

    Wrote tests for hazard model

--- spec/models/hazard_spec.rb
+++ spec/models/hazard_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Hazard do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:hazard) { Hazard.create }
+  subject { hazard }
+  
+  it { should validate_presence_of(:latitude) }
+  it { should validate_presence_of(:longitude) }
+  it { should validate_presence_of(:hazard_type) }
+  it { should validate_presence_of(:user_id) }
 end
