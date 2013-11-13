08b8ae4d35f478135255d46051e3d7608f8f030c
fo95116 <fo95116@gmail.com>
2013-11-13 11:07:03 -0800

    one succesful test another commented out for more input

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.create( name: 'paco orozco', provider: 'facebook', uid: '2345')}
+  let(:user) { User.create(name: 'paco orozco', provider: 'twitter', uid: '2345')}
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:provider) }
-  it { should validate_presence_of(:uid)}
+    it { should have_many(:hazards) }
+
+  # describe 'self.create_with_omniauth(auth)' do
+
+  #   it "should create a user with omniauth properties" do
+  #     test = User.new
+  #     user.provider
+
+  #   end
+
+
+  # end
+
+
 
 end
