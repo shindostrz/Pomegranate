a9f793303820bb78212552707ef766b0f7f27e02
fo95116 <fo95116@gmail.com>
2013-11-13 14:18:41 -0800

    moar testing for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,17 +4,21 @@ describe User do
   let(:user) { User.create(name: 'paco orozco', provider: 'twitter', uid: '2345')}
 
     it { should have_many(:hazards) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:provider) }
+    it { should validate_presence_of(:uid) }
+    it { should validate_uniqueness_of(:uid) }
 
-  # describe 'self.create_with_omniauth(auth)' do
-
+  describe 'self.create_with_omniauth(auth)' do
+    let(:user) { create(:user, :with_omniauth }
   #   it "should create a user with omniauth properties" do
   #     test = User.new
   #     user.provider
 
-  #   end
+      # end
 
 
-  # end
+  end
 
 
 
