c728cee36c44ff5aae07c277650a253a974e339f
Amy MacKinnon <amy@amy-mac.com>
2013-11-14 11:14:35 -0800

    Moved accident form to index page

--- app/controllers/accidents_controller.rb
+++ app/controllers/accidents_controller.rb
@@ -1,12 +1,11 @@
 class AccidentsController < ApplicationController
+  
   def index
-
-    @accident = Accident.new
     @accidents = Accidents.all
 
     respond_to do |format|
       format.html
-      format.json { render :json => @hazards }
+      format.json { render :json => @accidents }
     end
 
   end
@@ -16,7 +15,6 @@ class AccidentsController < ApplicationController
     @user = current_user
     @new_accident = @user.accidents.create(params[:accident])
     @marker = "#{@new_accident['latitude']}, #{@new_accident['longitude']}"
-    p @marker
 
     respond_to do |format|
       format.js
@@ -25,12 +23,7 @@ class AccidentsController < ApplicationController
   end
 
   def destroy
-    Hazard.delete(params[:id])
-    @id = params[:id]
 
-    respond_to do |format|
-      format.js
-    end
   end
 
 end
