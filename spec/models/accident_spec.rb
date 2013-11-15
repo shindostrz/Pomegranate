3e0a36c565f007cb59049a2bf031f0e2030e79f5
fo95116 <fo95116@gmail.com>
2013-11-15 15:47:29 -0800

    some moar styling, feel free to give feedback

--- spec/models/accident_spec.rb
+++ spec/models/accident_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe Accident do
   let(:accident) { Accident.create(latitude: '37.785752', longitude: '122.412758',
-    details: 'construction in soma', news_url: 'example.com', user_id: 1)}
+    details: 'construction in soma', news_url: 'example.com', user_id: 1, accident_date: '2013-11-12')}
 
   it { should belong_to(:user)}
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
   it { should validate_presence_of(:details) }
+  it { should validate_presence_of(:accident_date) }
 
 end
