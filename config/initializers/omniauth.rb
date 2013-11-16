0b98376e4684d93b2828121299577607831b68af
fo95116 <fo95116@gmail.com>
2013-11-16 15:13:37 -0800

    commit with bug fix attempt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
+
+# OmniAuth.config.on_failure = Proc.new { |env|
+#   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+# }
