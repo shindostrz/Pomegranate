ff3405445cf665970d6359c693ce0b4d8e39b547
Amy MacKinnon <amy@amy-mac.com>
2013-11-17 11:38:46 -0800

    Moved jquery from application.js to accidents.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,28 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+//= require accidents
 //= require markerclusterer
 //= require hazards
 //= require sessions
-//= require_self
-
-//Disclosure widget for form
-$(document).ready(function() {
-
-  $('.form_disclosure').on('click', function(event) {
-    event.preventDefault();
-    $(this).next('div').toggleClass('hidden');
-    $(this).find('.arrow').toggleClass('arrow-toggle');
-  });
-
-  $('#close-sidebar').on('click', function(event) {
-    event.preventDefault();
-    $('#sidebar').addClass('hidden');
-  });
-
-  $('#close').on('click', function(event) {
-    event.preventDefault();
-    $('#popup').hide();
-  });
-
-});
