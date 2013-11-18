a574191c7c868d45ed15852eded93b03fde9b8df
Amy MacKinnon <amy@amy-mac.com>
2013-11-17 19:01:02 -0800

    Made modal forms look prettier

--- app/assets/javascripts/accidents.js
+++ app/assets/javascripts/accidents.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
 
   $('.form_disclosure').on('click', function(event) {
     event.preventDefault();
-    $(this).next('div').toggleClass('hidden');
-    $(this).find('.arrow').toggleClass('arrow-toggle');
+    // $(this).next('div').toggleClass('hidden');
+    // $(this).find('.arrow').toggleClass('arrow-toggle');
+    $('.form_disclosure').next('div').toggleClass('hidden');
+    $('.form_disclosure').find('.arrow').toggleClass('arrow-toggle');
   });
 
   $('#close-sidebar').on('click', function(event) {
