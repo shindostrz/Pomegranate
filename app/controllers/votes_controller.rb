0c6916420226c5f9bce9691db2229783e409a25b
Amy MacKinnon <amy@amy-mac.com>
2014-01-15 15:19:40 -0800

    Made the vote.create simpler

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,10 +14,7 @@ class VotesController < ApplicationController
 
     def create
       @user = current_user
-      @hazard = Hazard.find(params[:hazard_id])
-      @vote = @user.votes.create(voted: params[:vote])
-      @hazard.votes << @vote
-      @vote = Vote.last
+      @vote = @user.votes.create(voted: params[:vote], hazard_id: params[:hazard_id])
       render json: @vote, status: 201
     end
 
