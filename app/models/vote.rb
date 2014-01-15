2bfc048612170d108c43719d18ff5868e2296ba8
Amy MacKinnon <amy@amy-mac.com>
2014-01-15 14:25:37 -0800

    Added in a method to call the vote refresh worker

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,12 @@
 class Vote < ActiveRecord::Base
   attr_accessible :voted, :user_id, :hazard_id
+  after_commit :count_the_votes, on: :create
 
   belongs_to :user
   belongs_to :hazard
+
+  def count_the_votes
+    VoteRefreshWorker.perform_async(self.hazard_id)
+  end
 
 end
